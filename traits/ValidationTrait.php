b1238e5e47790fb958748b1fb03780b081a7098b
Simon Siva <simonsiva13@gmail.com>
2022-11-02 06:39:00 +0300

    add validation trait

---
+++ traits/ValidationTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+
+/**
+ * Created by ssiva on 02/11/2022
+ */
+
+
+namespace app\traits;
+
+use Carbon\Carbon;
+
+trait ValidationTrait
+{
+    public function beforeValidate()
+    {
+        if(!$this->isNewRecord){
+            $this->date_created = ($this->date_created) ? Carbon::parse($this->date_created) : $this->date_created;
+            $this->date_modified = ($this->date_modified) ? Carbon::parse($this->date_modified) : $this->date_modified;
+        }
+        return parent::beforeValidate();
+    }
+}
