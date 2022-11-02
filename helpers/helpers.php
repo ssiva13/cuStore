4532ab2eb9d6750c92ad0b506ccb129e86e6f1de
Simon Siva <simonsiva13@gmail.com>
2022-11-02 05:38:56 +0300

    add string manipulation methods

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -14,3 +14,16 @@ if (!function_exists('id2camel')) {
         return Inflector::id2camel($string);
     }
 }
+if (!function_exists('slug')) {
+    function slug($string): string
+    {
+        return Inflector::slug($string);
+    }
+}
+if (!function_exists('id2human')) {
+    function id2human($string): string
+    {
+        $camel = Inflector::id2camel($string);
+        return Inflector::camel2words($camel);
+    }
+}
